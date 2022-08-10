31e6a9e5b86b307d2b51093c81d401b91bca779a
nicolas.prokopetz <nicolas@prokopetz.com.br>
2022-08-10 20:04:34 -0300

    add glm

--- tarefa1/main.cpp
+++ tarefa1/main.cpp
@@ -1,5 +1,8 @@
 #include <GL/glew.h>    // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <stdio.h>
 
 int main()
@@ -29,74 +32,87 @@ int main()
   glewExperimental = GL_TRUE;
   glewInit();
 
-  // get version info
-  const GLubyte *renderer = glGetString(GL_RENDERER); // get renderer string
-  const GLubyte *version = glGetString(GL_VERSION);   // version as a string
+  const GLubyte *renderer = glGetString(GL_RENDERER);
+  const GLubyte *version = glGetString(GL_VERSION);
   printf("Renderer: %s\n", renderer);
   printf("OpenGL version supported %s\n", version);
 
-  // tell GL to only draw onto a pixel if the shape is closer to the viewer
-  glEnable(GL_DEPTH_TEST); // enable depth-testing
-  glDepthFunc(GL_LESS);    // depth-testing interprets a smaller value as "closer"
+  GLfloat vertices[] = {
+      // Positions          // Colors
+      0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // Bottom Right
+      -0.5f, -0.5f, -0.0f, 0.0f, 0.0f, 1.0f, // Bottom Left
+      0.0f, 0.5f, 0.0f, 0.0f, 0.5f, 0.5f     // Top
+  };                                         // 1 VBO e 1 VAO
 
-  float points[] = {
-      0.0f, 0.5f, 0.0f,
-      0.5f, -0.5f, 0.0f,
-      -0.5f, -0.5f, 0.0f};
-
-  GLuint vbo = 0;
-  glGenBuffers(1, &vbo);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
-
-  GLuint vao = 0;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
+  GLuint VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),
+               vertices, GL_STATIC_DRAW);
+  // Posições:
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                        6 * sizeof(GLfloat), (GLvoid *)0);
   glEnableVertexAttribArray(0);
-  glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+  // Cores:
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
+                        6 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
 
   const char *vertex_shader =
-      "#version 400\n"
-      "in vec3 vp;"
+      "#version 410\n"
+      "layout(location=0) in vec3 vp;"
+      "layout(location=1) in vec3 vc;"
+      "out vec3 color;"
+      "uniform mat4 transform;"
       "void main() {"
-      "  gl_Position = vec4(vp, 1.0);"
+      "  color = vc;"
+      "  gl_Position = transform * vec4(vp, 1.0);"
       "}";
 
   const char *fragment_shader =
-      "#version 400\n"
-      "out vec4 frag_colour;"
+      "#version 410\n"
+      "in vec3 color;"
+      "out vec4 frag_color;"
       "void main() {"
-      "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+      "  frag_color = vec4(color, 1.0);"
       "}";
 
   GLuint vs = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vs, 1, &vertex_shader, NULL);
   glCompileShader(vs);
+
   GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fs, 1, &fragment_shader, NULL);
   glCompileShader(fs);
 
-  GLuint shader_programme = glCreateProgram();
-  glAttachShader(shader_programme, fs);
-  glAttachShader(shader_programme, vs);
-  glLinkProgram(shader_programme);
+  GLuint program = glCreateProgram();
+  glAttachShader(program, fs);
+  glAttachShader(program, vs);
+
+  glLinkProgram(program);
+
+  glUseProgram(program);
 
   while (!glfwWindowShouldClose(window))
   {
-    // wipe the drawing surface clear
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glUseProgram(shader_programme);
-    glBindVertexArray(vao);
-    // draw points 0-3 from the currently bound VAO with current in-use shader
+
+    glm::mat4 transform = glm::translate(glm::mat4(1.0f), glm::vec3(1.0f));
+
+    glUseProgram(program);
+    unsigned int transformLoc = glGetUniformLocation(program, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+    glBindVertexArray(VAO);
+
     glDrawArrays(GL_TRIANGLES, 0, 3);
-    // update other events like input handling
-    glfwPollEvents();
-    // put the stuff we've been drawing onto the display
+
     glfwSwapBuffers(window);
+    glfwPollEvents();
   }
 
-  // close GL context and any other GLFW resources
   glfwTerminate();
   return 0;
 }
