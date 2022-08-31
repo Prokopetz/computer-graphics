278b4a5d5aeeb0340467762cb163c09dc47aa712
nicolas.prokopetz <nicolas@prokopetz.com.br>
2022-08-31 20:53:23 -0300

    fix screen saver

--- screenSaver/main.cpp
+++ screenSaver/main.cpp
@@ -129,22 +129,57 @@ int main()
     static double previousSeconds = glfwGetTime();
     double currentSeconds = glfwGetTime();
     double elapsedSeconds = currentSeconds - previousSeconds;
-    if (elapsedSeconds > 0.008)
+    if (elapsedSeconds > 0.008f)
     {
       previousSeconds = currentSeconds;
-      if (fabs(lastPositionX) + 0.5f >= 1.0f)
+      if (elapsedSeconds > 0.008)
       {
-        speedX = -speedX;
-      }
-      else if (fabs(lastPositionY) + 0.5f >= 1.0f)
-      {
-        speedY = -speedY;
-      }
+        previousSeconds = currentSeconds;
+        float nX;
+        float nY;
+        bool collision = false;
 
-      matrix[12] = elapsedSeconds * speedX + lastPositionX;
-      matrix[13] = elapsedSeconds * speedY + lastPositionY;
-      lastPositionX = matrix[12];
-      lastPositionY = matrix[13];
+        if (lastPositionX + 0.5f > 1.0f) // right
+        {
+          nX = -1.0f;
+          nY = 0.0f;
+          collision = true;
+        }
+        else if (lastPositionY + 0.5f > 1.0f) // top
+        {
+          nX = 0.0f;
+          nY = -1.0f;
+          collision = true;
+        }
+        else if (lastPositionX - 0.5f < -1.0f) // left
+        {
+          nX = 1.0f;
+          nY = 0.0f;
+          collision = true;
+        }
+        else if (lastPositionY - 0.5f < -1.0f) // bottom
+        {
+          nX = 0.0f;
+          nY = 1.0f;
+          collision = true;
+        }
+
+        if (collision)
+        {
+          float r2X = -speedX;
+          float r2Y = -speedY;
+          float a = (nX * r2X + nY * r2Y);
+          float routX = 2 * nX * a - r2X;
+          float routY = 2 * nY * a - r2Y;
+          speedX = routX;
+          speedY = routY;
+        }
+
+        matrix[12] = elapsedSeconds * speedX + lastPositionX;
+        matrix[13] = elapsedSeconds * speedY + lastPositionY;
+        lastPositionX = matrix[12];
+        lastPositionY = matrix[13];
+      }
     }
 
     glUniformMatrix4fv(matrixLocation, 1, GL_FALSE, matrix);
