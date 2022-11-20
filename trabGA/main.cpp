67e86af55501ae0afc800ddbbfdb668471c09e7a
joaodacolsoares <joao.dacol.soares@gmail.com>
2022-11-20 16:37:23 -0300

    refactor: a bunch of stuff

--- trabGA/main.cpp
+++ trabGA/main.cpp
@@ -1,42 +1,267 @@
 #define STB_IMAGE_IMPLEMENTATION
 
-#include <GL/glew.h>    // include GLEW and new version of GL on Windows
-#include <GLFW/glfw3.h> // GLFW helper library
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <math.h>
+#include <string.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <stdio.h>
 
-#include "Scene.h"
-#include "models/Texture.h"
-#include "objects/Backpack.h"
-#include "objects/Ground.h"
-#include "objects/Player.h"
-#include "objects/Bullet.h"
+#include "newModel/NewShader.h"
+#include "newModel/NewMesh.h"
+#include "newModel/NewMaterial.h"
+#include "newModel/NewGroup.h"
+#include "newModel/NewFace.h"
+#include "utils/NewObjectReader.h"
 
 int main()
 {
-  Scene *scene = new Scene(800, 600, "Quake");
-  scene->init();
+  const GLint WIDTH = 600, HEIGHT = 600;
+  GLFWwindow* window;
 
-  Player *player = new Player(scene->camera);
-  scene->addPlayer(player);
+  glfwInit();
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE );
+	glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
+	glfwWindowHint( GLFW_SAMPLES, 4 );
 
-  Texture *groundTexture = new Texture();
-  groundTexture->load("./trabGA/assets/ground.jpg", scene->shader->program);
+	window = glfwCreateWindow( WIDTH, HEIGHT, "Trabalho Grau B", nullptr, nullptr );
 
-  Texture *backpackTexture = new Texture();
-  backpackTexture->load("./trabGA/assets/backpack.jpg", scene->shader->program);
+	glfwGetFramebufferSize( window, 0, 0 );
 
-  Texture *bulletTexture = new Texture();
-  bulletTexture->load("./trabGA/assets/mesa01.bmp", scene->shader->program);
+	if ( window == nullptr ) {
+		glfwTerminate();
 
-  Ground *ground = new Ground(groundTexture);
-  Backpack *backpack = new Backpack(backpackTexture);
+		return EXIT_FAILURE;
+	}
 
-  backpack->setPosition(glm::vec3(0.0f, 2.2f, -10.5f));
-  scene->addObject(backpack);
-  scene->addObject(ground);
+	glfwMakeContextCurrent( window );
 
-  return scene->run();
+	glEnable(GL_DEPTH_TEST);
+	glClear(GL_DEPTH_BUFFER_BIT);
+
+	glewExperimental = GL_TRUE;
+
+	if ( glewInit() != GLEW_OK ) {
+		return EXIT_FAILURE;
+	}
+
+	glViewport( 0, 0, 800, 600 );
+
+  glEnable( GL_BLEND );
+	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+
+	glEnable( GL_DEPTH_TEST );
+
+  Shader coreShader = Shader( "./trabGA/shaders/vertex.shader", "./trabGA/shaders/fragment.shader" );
+	coreShader.Use();
+
+	////////
+	////////
+	////////
+	////////
+
+	 glm::ortho(0.0f, (float) WIDTH, 0.0f, (float) HEIGHT, 0.1f, 100.0f);
+
+	glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float) WIDTH / (float) HEIGHT, 0.1f, 100.0f);
+
+	coreShader.setMatrix4fv("projection", proj);
+
+	vector<NewMesh*> meshs;
+	meshs.push_back(NewObjectReader::read("./trabGA/assets/trout/pista.obj"));
+	// vector<vec3*> translatePoints;
+
+	for (NewMesh* mesh : meshs) {
+		for (NewGroup* group : mesh->getGroups()) {
+
+			NewMaterial* material = mesh->getMaterial(group->getMaterial());
+			coreShader.LoadTexture(strdup(material->getTexture().c_str()), "texture1", group->getName());
+			coreShader.setVec3("materialAmbient", vec3(material->getAmbient()->x, material->getAmbient()->y, material->getAmbient()->z));
+			coreShader.setVec3("materialDiffuse", vec3(material->getDiffuse()->x, material->getDiffuse()->y, material->getDiffuse()->z));
+			coreShader.setVec3("materialSpecular", vec3(material->getSpecular()->x, material->getSpecular()->y, material->getSpecular()->z));
+			coreShader.setFloat("materialShininess", material->getShininess());
+
+			vector<float> vertices;
+			vector<float> normais;
+			vector<float> textures;
+
+			for (NewFace* face : group->getFaces()) {
+				for (int verticeID : face->getVertices()) {
+					glm::vec3* vertice = mesh->vertice(verticeID - 1);
+					vertices.push_back(vertice->x);
+					vertices.push_back(vertice->y);
+					vertices.push_back(vertice->z);
+
+					group->increaseNumVertices();
+				}
+
+				for (int normalID : face->getNormais()) {
+					glm::vec3* normal = mesh->normal(normalID - 1);
+					normais.push_back(normal->x);
+					normais.push_back(normal->y);
+					normais.push_back(normal->z);
+				}
+
+				for (int textureID : face->getTextures()) {
+					glm::vec2* texture = mesh->texture(textureID - 1);
+					textures.push_back(texture->x);
+					textures.push_back(texture->y);
+				}
+			}
+
+			GLuint VBOvertices, VBOnormais, VBOtextures, VAO;
+			glGenVertexArrays(1, &VAO);
+			glGenBuffers(1, &VBOvertices);
+			glGenBuffers(1, &VBOnormais);
+			glGenBuffers(1, &VBOtextures);
+
+			glBindVertexArray(VAO);
+
+			glBindBuffer(GL_ARRAY_BUFFER, VBOvertices);
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+			glEnableVertexAttribArray(0);
+
+			glBindBuffer(GL_ARRAY_BUFFER, VBOnormais);
+			glBufferData(GL_ARRAY_BUFFER, normais.size() * sizeof(float), normais.data(), GL_STATIC_DRAW);
+
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+			glEnableVertexAttribArray(1);
+
+			glBindBuffer(GL_ARRAY_BUFFER, VBOtextures);
+			glBufferData(GL_ARRAY_BUFFER, textures.size() * sizeof(float), textures.data(), GL_STATIC_DRAW);
+
+			glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid*)0);
+			glEnableVertexAttribArray(2);
+
+			group->setVAO(&VAO);
+			glBindVertexArray(0); 
+		}
+	}
+
+	float angle = 135.0f;
+	int translatePoint = 0;
+
+	float camX = 1.0f;
+	float camY = 0.5f;
+	float camZ = 1.0f;
+
+	float deltaTime;
+	float lastFrame = 0.0f;
+	
+	while ( !glfwWindowShouldClose( window ) ) {
+
+		glfwPollEvents();
+
+		if ( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS ) {
+			glfwSetWindowShouldClose( window, GLFW_TRUE );
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+			float currentFrame = glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+			
+			if (deltaTime > 0.1f) {
+				lastFrame = currentFrame;
+				translatePoint = translatePoint - 1;
+			}
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+			float currentFrame = glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+
+			if (deltaTime > 0.1f) {
+				lastFrame = currentFrame;
+				translatePoint = translatePoint + 1;
+			}
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+			angle += 0.1f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+			angle -= 0.1f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+			camY += 0.1f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+			camY -= 0.1f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+			camX += 0.1f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+			camX -= 0.1f;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+			camX = 1.0f;
+			camY = 0.5f;
+			camZ = 1.0f;
+		}
+
+		glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+		
+		coreShader.Use();
+
+		glm::mat4 view(1.0f);
+		view = glm::lookAt(glm::vec3(camX, camY, camZ),
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f));
+
+		coreShader.setMatrix4fv("view", view);
+
+		coreShader.setVec3("lightColor", vec3(1.0f, 1.0f, 1.0f));
+		coreShader.setVec3("lightPos", vec3(-100.0f, 1.0f, 100.0f));
+		coreShader.setVec3("viewPos", vec3(camX, camY, camZ));
+
+		// float translateX = translatePoints[translatePoint % translatePoints.size()]->x * -1;
+		// float translateY = 0.0f;
+		// float translateZ = translatePoints[translatePoint % translatePoints.size()]->z * -1;
+
+		glm::mat4 model(1.0f);
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+		// model = glm::translate(model, glm::vec3(translateX, translateY, translateZ));
+
+		coreShader.setMatrix4fv("model", model);
+		for (NewMesh* mesh : meshs) {
+			for (NewGroup* group : mesh->getGroups()) {
+				coreShader.UseTexture(group->getName());
+				glBindVertexArray(group->getVAO());
+				glDrawArrays(GL_TRIANGLES, 0, group->getNumVertices());
+				glBindVertexArray(0);
+			}
+		}
+
+		glfwSwapBuffers(window);
+	}
+
+	////////
+	////////
+	////////
+
+	return EXIT_SUCCESS;
 }
---
+++ trabGA/shaders/fragment.shader
@@ -0,0 +1,37 @@
+#version 330 core
+
+in vec2 TexCoord;
+in vec3 Normal;
+in vec3 FragPos;
+
+out vec4 color;
+
+uniform vec3 lightPos; 
+uniform vec3 viewPos; 
+uniform vec3 lightColor;
+
+uniform vec3 materialAmbient;
+uniform vec3 materialDiffuse;
+uniform vec3 materialSpecular;
+uniform float materialShininess;
+
+uniform sampler2D texture1;
+
+void main() {
+	vec4 objectColor = texture(texture1, TexCoord);
+
+    vec3 ambient = lightColor * materialAmbient;
+  	
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor * materialDiffuse;
+    
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), materialShininess);
+    vec3 specular = spec * lightColor * materialSpecular;  
+        
+    vec3 result = ambient + diffuse + specular;
+	color = vec4(result, 1.0) * objectColor;
+}
